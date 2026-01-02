a734c3d3d5c9e67afca9f4c4c37b691c19dcdd77
Dave Spencer Bacay <spencerbacay@gmail.com>
2026-01-02 23:02:50 +0800

    feat: creation of UI components

---
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Modells.Domain;
+using CodePulse.API.Modells.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    // api/categories
+    [Route("api/category")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+        //
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Map DTO to Domain Model
+            var category = new Category
+            { 
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await categoryRepository.CreateAsync(category);
+
+            // Domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ API/CodePulse.API/CodePulse.API/Modells/DTO/CategoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CodePulse.API.Modells.Domain
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
+
+
